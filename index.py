e2f69efabbc60774b26eb5f18af2d911dc1cb925
Your Name <hqnga1604@gmail.com>
2022-11-01 21:59:26 +0700

    main

--- index.py
+++ index.py
@@ -2,10 +2,13 @@ from flask import render_template, request
 from __init__ import app
 
 
-@app.route("/")
+@app.route("/index/")
 def Hello():
     return render_template('index.html')
 
+@app.route("/dsbs/")
+def Bac_si():
+    return render_template('dsbs.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
