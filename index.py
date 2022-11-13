2420c4c11dcd70c36ffb0ea8e8465fc6560ebc89
your name <luonghuy148894@gmail.com>
2022-11-13 11:34:39 +0700

    add

--- index.py
+++ index.py
@@ -8,6 +8,41 @@ def index():
     return render_template('index.html')
 
 
+@app.route("/dklk")
+def dklk():
+    return render_template("dklk.html")
+
+@app.route("/dsbs")
+def dsbs():
+    return render_template("dsbs.html")
+
+@app.route("/dangnhap")
+def dangnhap():
+    return render_template("dangnhap.html")
+
+@app.route("/dn", methods = ["POST","GET"])
+def dn():
+    if request.method == "POST":
+        user_name = request.form["name"]
+        user_password = request.form["password"]
+
+        if user_name:
+            session["user"] = user_name
+            session["password"] = user_password
+
+
+            return redirect("/dk")
+    return render_template("dn.html")
+
+@app.route("/dk")
+def dk():
+    if "user"and"password" in session:
+        name = session["user"]
+        password = session["password"]
+        return f"<h1>Đăng kí thành công!!</h1>"
+    else:
+        return redirect(url_for("dn"))
+
 @app.route('/login-admin', methods=['post'])
 def login_admin():
     username = request.form['username']
